b397a341355614b8375eded169c903207d0f0325
Harout Hedeshian <harouth@codeaurora.org>
2013-07-01 14:08:32 -0600

    netd: enable IPv6 forwarding and handle upstreaminterface
    This patch modifies netd to receive addUpstreamInterface and
    removeUpstreamInterface commands from the tethering java
    framework. Adds v6start/v6stop which calls the ICMPv6 forwarder
    with the appropriate interfaces. Enables IPv6 forwarding.
    
    Change-Id: Ic857f15ea9c5ec4be0dbde2656b55e65060f4497

--- TetherController.h
+++ TetherController.h
@@ -57,6 +57,9 @@ public:
     int removeV6RtrAdvIface(const char *iface);
     int addUpstreamInterface(char *iface);
     int removeUpstreamInterface(char *iface);
+
+private:
+    int applyDnsInterfaces();
 };
 
 #endif
